c91eddbdbcae0ffd3389288e761c6fc21c79bef1
azurize <46197517+azurize@users.noreply.github.com>
2023-05-24 20:24:50 -0700

    Various updates

--- src/card.js
+++ src/card.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./card.css";
 
 const Card = ({ name, email, id }) => {
     return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="el robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
 export const robots = [
   {
     id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
+    name: 'Easy Pete',
+    username: 'ezpete',
+    email: 'dynamite@goodsprings.gov'
   },
   {
     id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv'
+    name: 'Cap Hunter Matty',
+    username: 'capHunter',
+    email: 'caps@waste.land'
   },
   {
     id: 3,
-    name: 'Clementine Bauch',
-    username: 'Samantha',
-    email: 'Nathan@yesenia.net'
+    name: 'Victoria Nuka Cola',
+    username: 'nukacolavic',
+    email: 'victoria@nuka.cola'
   },
   {
     id: 4,
-    name: 'Patricia Lebsack',
-    username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
+    name: 'ArcJet Dolly',
+    username: 'arcjetdol',
+    email: 'dolly@arcjet.net'
   },
   {
     id: 5,
-    name: 'Chelsey Dietrich',
-    username: 'Kamren',
-    email: 'Lucio_Hettinger@annie.ca'
+    name: 'Gary',
+    username: 'hahahagary',
+    email: 'gaarryy@vault103.com'
   },
   {
     id: 6,
-    name: 'Mrs. Dennis Schulist',
-    username: 'Leopoldo_Corkery',
-    email: 'Karley_Dach@jasper.info'
+    name: 'Col. Augustus Autumn',
+    username: 'aautumn',
+    email: 'aautumn@enclave.gov'
   },
   {
     id: 7,
-    name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz'
+    name: 'Nick Valentine',
+    username: 'detectivenick',
+    email: 'nickyv@diamondcity.gov'
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
-    username: 'Maxime_Nienow',
-    email: 'Sherwood@rosamond.me'
+    name: 'Dave',
+    username: 'daveleader',
+    email: 'leader@republicofdave.gov'
   },
   {
     id: 9,
-    name: 'Glenna Reichert',
-    username: 'Delphine',
-    email: 'Chaim_McDermott@dana.io'
+    name: 'Rex',
+    username: 'bark',
+    email: 'newdog@ontheblock.com'
   },
   {
     id: 10,
-    name: 'Clementina DuBuque',
-    username: 'Moriah.Stanton',
-    email: 'Rey.Padberg@karina.biz'
+    name: 'Arcade Gannon',
+    username: 'arcremnants',
+    email: 'arcade@old.enclave.gov'
   }
 ];
